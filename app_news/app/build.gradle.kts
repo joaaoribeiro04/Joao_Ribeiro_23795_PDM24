850b16962b73c738ef66bdd1c588404b69f2549a
João Ribeiro <jpmr0703@gmail.com>
2025-01-07 15:42:12 +0000

    add images in news app

--- app_news/app/build.gradle.kts
+++ app_news/app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.compose.ui:ui:1.4.3")
     implementation("androidx.compose.material:material:1.4.3")
+    implementation("io.coil-kt:coil-compose:2.2.2")
 }
--- app_news/app/src/main/java/com/example/nytimesnewsapp/NewsListScreen.kt
+++ app_news/app/src/main/java/com/example/nytimesnewsapp/NewsListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.nytimesnewsapp
 
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,9 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.nytimesnewsapp.model.NewsArticle
 import com.example.nytimesnewsapp.viewmodel.NewsViewModel
+import androidx.navigation.NavController
+
 
 @Composable
 fun NewsListScreen(viewModel: NewsViewModel, navController: NavController) {
@@ -30,6 +33,18 @@ fun NewsListScreen(viewModel: NewsViewModel, navController: NavController) {
 @Composable
 fun NewsItem(news: NewsArticle, onReadMoreClick: () -> Unit) {
     Column(modifier = Modifier.padding(8.dp)) {
+        news.multimedia?.let {
+            val imageUrl = it.firstOrNull()?.url
+            imageUrl?.let {
+                Image(
+                    painter = rememberImagePainter(imageUrl),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth().height(200.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+
         Text(text = news.title, style = MaterialTheme.typography.titleLarge)
         Text(text = news.abstract)
         Button(onClick = onReadMoreClick) {
